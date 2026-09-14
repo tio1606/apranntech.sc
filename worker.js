2b7f00ab3d6122e63fda094b189fe2965eb016ba
tio1606 <digitalie.sc@gmail.com>
2026-09-15 00:32:15 +0400

    Show API error for registration diagnosis

--- worker.js
+++ worker.js
@@ -844,7 +844,7 @@ export default {
         return json(
           {
             error:
-              "Server error."
+            error?.message || "Server error."
           },
           500
         );
