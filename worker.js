e72a0ebd7f173a19b04532b1385c185844f356c3
tio1606 <digitalie.sc@gmail.com>
2026-09-26 21:05:45 +0400

    Trigger Cloudflare deployment for profile fix

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+/* Cloudflare deployment trigger - profile fix */
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
